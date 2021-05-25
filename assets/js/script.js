9653da47681d338648b76d6987ec639b48160791
Zi-on <zioniflores12@yahoo.com>
2021-05-25 03:15:00 -0500

    add my js code so my first coloumn displays the appropiate color

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,19 @@ $("#currentDay").text(today.format("dddd, MMMM Do"));
 
 console.log(today);
 
+var currentTime = today.format("h a")
+
+var nineAm = moment("9 am", "h a").format("h a");
+
+if (nineAm == currentTime){
+   var nine = $('.nine').addClass('present')
+}else if (nineAm > currentTime) {
+    var nine = $('.nine').addClass('future')
+}
+else {
+    var nine = $('.nine').addClass('past')
+}
+
+
+console.log(currentTime)
+console.log(nineAm)
