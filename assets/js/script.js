6a79f2036cd3b5f1daaa29f3e56c4039f7317db8
Zi-on <zioniflores12@yahoo.com>
2021-05-26 14:09:38 -0500

    refacrored my update funtion

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,79 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
-console.log(today);
 
-var currentTime = today.format("h a")
 
-var nineAm = moment("9 am", "h a").format("h a");
+var currentTime = today.format("H")
 
-if (nineAm == currentTime){
-   var nine = $('.nine').addClass('present')
-}else if (nineAm > currentTime) {
-    var nine = $('.nine').addClass('future')
+
+function update(){
+if (9 == currentTime) {
+  $(".nine").addClass("present");
+} else if (9 > currentTime) {
+  $(".nine").addClass("future");
+} else {
+  $(".nine").addClass("past");
 }
-else {
-    var nine = $('.nine').addClass('past')
+if (10 == currentTime) {
+  $(".ten").addClass("present");
+} else if (10 > currentTime) {
+  $(".ten").addClass("future");
+} else if (10 < currentTime) {
+  $(".ten").addClass("past");
 }
+if (11 == currentTime) {
+  $(".eleven").addClass("present");
+} else if (11 > currentTime) {
+  $(".eleven").addClass("future");
+} else {
+  $(".eleven").addClass("past");
+}
+if (12 == currentTime) {
+  $(".twelve").addClass("present");
+} else if (12 > currentTime) {
+  $(".twelve").addClass("future");
+} else {
+  $(".twelve").addClass("past");
+}
+if (13 == currentTime) {
+  $(".one").addClass("present");
+} else if (13 > currentTime) {
+  $(".one").addClass("future");
+} else {
+  $(".one").addClass("past");
+}
+if (14 == currentTime) {
+  $(".two").addClass("present");
+} else if (14 > currentTime) {
+  $(".two").addClass("future");
+} else {
+  $(".two").addClass("past");
+}
+if (15 == currentTime) {
+  $(".three").addClass("present");
+} else if (15 > currentTime) {
+  $(".three").addClass("future");
+} else {
+  $(".three").addClass("past");
+}
+if (16 == currentTime) {
+ $(".four").addClass("present");
+} else if (16 > currentTime) {
+  $(".four").addClass("future");
+} else {
+  $(".four").addClass("past");
+}
+if (17 == currentTime) {
+  $(".five").addClass("present");
+} else if (17 > currentTime) {
+  $(".five").addClass("future");
+} else {
+  $(".five").addClass("past");
+}
+}
+update();
 
 
-console.log(currentTime)
-console.log(nineAm)
+
+
+
